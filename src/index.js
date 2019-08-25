916dae6a796a08fff48ffbf59bd1d99c93a6196a
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-08-24 22:11:59 -0300

    NEW - Reactotron, Redux e serviços

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 
+import './config/ReactotronConfig';
+
+import NavigationService from './services/navigation';
+
 import Routes from './routes';
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#25212e" />
-      <Routes />
+      <Routes
+        ref={navigatorRef => {
+          NavigationService.setTopLevelNavigator(navigatorRef);
+        }}
+      />
     </>
   );
 }
---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,67 @@
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { Alert } from 'react-native';
+
+import api from '~/services/api';
+import navigation from '~/services/navigation';
+
+import { signInSuccess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    if (user.provider) {
+      Alert.alert('Erro no login', 'Usuário não pode ser prestador');
+      return;
+    }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    yield put(signInSuccess(token, user));
+  } catch (err) {
+    Alert.alert('Falha na autenticação', 'Verifique seus dados');
+
+    yield put(signFailure());
+  }
+}
+
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+    });
+
+    navigation.navigate('SignIn');
+  } catch (err) {
+    Alert.alert('Falha no cadastro', 'Verifique seus dados');
+
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
